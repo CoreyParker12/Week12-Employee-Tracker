47e3a7436f5bc40cf72a7a00cdb325c2404651b6
Corey Parker <coreyparker012@gmail.com>
2021-07-13 18:55:54 -0400

    added base logic for entire option tree

--- employee.js
+++ employee.js
@@ -1,6 +1,5 @@
 const mysql = require('mysql');
 const inquirer = require('inquirer');
-const { exit } = require('process');
 
 const connection = mysql.createConnection({
     host: 'localhost',
@@ -17,6 +16,11 @@ const connection = mysql.createConnection({
     database: 'cmsDB',
 });
 
+
+// Functions inside:
+//  addFunction()
+//  viewFunction()
+//  updateFunction()
 const init = () => {
     inquirer
         .prompt({
@@ -28,22 +32,117 @@ const init = () => {
         .then((answer) => {
             switch(answer.homepage){
                 case 'ADD':
-                    console.log('ADD test');
                     addFunction();
                     break;
                 case 'VIEW':
-                    console.log('VIEW test');
                     viewFunction();
                     break;   
                 case 'UPDATE':
-                    console.log('UPDATAE test');
                     updateFunction();
                     break;  
                 default:
-                    console.log('Thank you, exiting program.')
-                    process.exit();
+                    exit();
             }
         });
+};
+
+// Functions inside:
+//  addEmployee()
+//  addRole()
+//  addDepartment() 
+const addFunction = () => {
+    inquirer
+        .prompt({
+            name: 'addHomepage',
+            type: 'rawlist',
+            message: 'Would you like to add a(n) [EMPLOYEE], [ROLE], or [DEPARTMENT]?',
+            choices: ['EMPLOYEE', 'ROLE', 'DEPARTMENT', 'EXIT'],
+        })
+        .then((answer) => {
+            switch(answer.addHomepage){
+                case 'EMPLOYEE':
+                    addEmployee();
+                    break;
+                case 'ROLE':
+                    addRole();
+                    break;   
+                case 'DEPARTMENT':
+                    addDepartment();
+                    break;  
+                default:
+                    exit();
+            }
+        });
+};
+
+
+// Functions inside:
+//  viewAll()
+//  viewEmployee()
+//  viewRole()
+//  viewDepartment()
+const viewFunction = () => {
+    inquirer
+        .prompt({
+            name: 'viewHomepage',
+            type: 'rawlist',
+            message: 'Would you like to view a(n) [ALL], [EMPLOYEE], [ROLE], or [DEPARTMENT]?',
+            choices: ['ALL', 'EMPLOYEE', 'ROLE', 'DEPARTMENT', 'EXIT'],
+        })
+        .then((answer) => {
+            switch(answer.viewHomepage){
+                case 'ALL':
+                    viewAll();
+                    break;
+                case 'EMPLOYEE':
+                    viewEmployee();
+                    break;
+                case 'ROLE':
+                    viewRole();
+                    break;   
+                case 'DEPARTMENT':
+                    viewDepartment();
+                    break;  
+                default:
+                    exit();
+            }
+        });
+};
+
+// Functions inside:
+//  updateRole()
+//  updateManager()
+//  updateDepartment()
+const updateFunction = () => {
+    inquirer
+        .prompt({
+            name: 'updateHomepage',
+            type: 'rawlist',
+            message: 'Would you like to update an employee [ROLE], [MANAGER], or [DEPARTMENT]?',
+            choices: ['ROLE', 'MANAGER', 'DEPARTMENT', 'EXIT'],
+        })
+        .then((answer) => {
+            switch(answer.updateHomepage){
+                case 'ROLE':
+                    updateRole();
+                    break;
+                case 'MANAGER':
+                    updateManager();
+                    break;
+                case 'DEPARTMENT':
+                    updateDepartment();
+                    break;   
+                default:
+                    exit();
+            }
+        });
+};
+
+
+// Function to exit from inquirer at the end of each switch statement
+const exit = () => {
+    console.log('Thank you, exiting program.')
+    process.exit();
 };
 
 init();
